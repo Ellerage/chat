3aae78a83b4e9f7b0e9955c4ea263c30a94be454
Artem164 <artem.lomaev@bravedevelopers.com>
2020-06-07 10:10:00 +0500

    add test controller

--- api/src/messages/message.module.ts
+++ api/src/messages/message.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MessageRepository } from './message.repository';
+import { MessagesController } from './messages.controller';
+import { MessagesService } from './messages.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MessageRepository])],
+  controllers: [MessagesController],
+  providers: [MessagesService],
 })
 export class MessageModule {}
