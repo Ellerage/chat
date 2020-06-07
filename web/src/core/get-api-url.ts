7c0782fa88985c90915c985250a7e025f47155b5
Artem164 <artem.lomaev@bravedevelopers.com>
2020-06-08 04:59:24 +0500

    change ip urls

--- web/src/core/get-api-url.ts
+++ web/src/core/get-api-url.ts
@@ -1,5 +1,5 @@
 export const getApiUrl = (path: string): string => {
-  return `http://localhost:5000/api/${path}`
+  return `http://172.31.32.80:5000/api/${path}`
 }
 
-export const WSUrl = "http://localhost:4000/ws"
+export const WSUrl = "http://172.31.32.80:4000/ws"
