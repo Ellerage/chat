f454481e3cad0bc6bf2ec3b7d4ddab9f1cc776f4
Artem164 <artem.lomaev@bravedevelopers.com>
2020-06-07 10:46:17 +0500

    web change api url

--- web/src/core/get-api-url.ts
+++ web/src/core/get-api-url.ts
@@ -1,5 +1,5 @@
 export const getApiUrl = (path: string): string => {
-  return `http://localhost:5000/api/${path}`
+  return `/api/${path}`
 }
 
-export const WSUrl = "http://localhost:4000/ws"
+export const WSUrl = "/ws"
