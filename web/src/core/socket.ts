04916934b01207608b9261a13fea471bb0fe6667
Artem164 <artem.lomaev@bravedevelopers.com>
2020-06-08 12:50:15 +0500

    Add log

--- web/src/core/socket.ts
+++ web/src/core/socket.ts
@@ -1,4 +1,6 @@
 import socketIOClient from "socket.io-client"
-import { getApiUrl, WSUrl } from "./get-api-url"
+import { WSUrl } from "./get-api-url"
 
 export const WS = socketIOClient(WSUrl)
+
+console.log(WS)
