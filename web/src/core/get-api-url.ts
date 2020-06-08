121c02ae2658f2627186069c7f754ceee5821620
Artem164 <artem.lomaev@bravedevelopers.com>
2020-06-08 05:08:22 +0500

    build nginx

--- web/src/core/get-api-url.ts
+++ web/src/core/get-api-url.ts
@@ -1,5 +1,5 @@
 export const getApiUrl = (path: string): string => {
-  return `http://172.31.32.80:5000/api/${path}`
+  return `/api/${path}`
 }
 
-export const WSUrl = "http://172.31.32.80:4000/ws"
+export const WSUrl = "/ws"
