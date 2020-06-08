ae232bd6d18751156bf9030259369e22df2ac1ff
Artem164 <artem.lomaev@bravedevelopers.com>
2020-06-08 08:14:44 +0500

    change url

--- web/public/server.js
+++ web/public/server.js
@@ -6,7 +6,7 @@ const path = require("path")
 app.use(express.static(__dirname))
 
 app.get("/", function (req, res) {
-  response.writeHead(200, { "Content-Type": "text/html" })
+  res.writeHead(200, { "Content-Type": "text/html" })
 
   res.sendFile(path.join(__dirname, "index.html"))
 })
--- web/src/core/get-api-url.ts
+++ web/src/core/get-api-url.ts
@@ -2,4 +2,4 @@ export const getApiUrl = (path: string): string => {
   return `/api/${path}`
 }
 
-export const WSUrl = "http://18.185.117.191:4000/ws"
+export const WSUrl = "http://18.185.117.191:4000"
