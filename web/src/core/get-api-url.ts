a4d868933aa4b8606fbd09e4325fc3af055365a7
Artem164 <artem.lomaev@bravedevelopers.com>
2020-06-08 06:13:00 +0500

    nginx url

--- web/src/core/get-api-url.ts
+++ web/src/core/get-api-url.ts
@@ -1,6 +1,5 @@
 export const getApiUrl = (path: string): string => {
-  return `http://ec2-18-185-117-191.eu-central-1.compute.amazonaws.com/api/${path}`
+  return `/api/${path}`
 }
 
-export const WSUrl =
-  "http://ec2-18-185-117-191.eu-central-1.compute.amazonaws.com/ws"
+export const WSUrl = "/ws"
