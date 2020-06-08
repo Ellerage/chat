00806b5ca2f4e581db519a30c24d3d48c4a69224
Artem164 <artem.lomaev@bravedevelopers.com>
2020-06-08 05:28:46 +0500

    deploy

--- web/src/core/get-api-url.ts
+++ web/src/core/get-api-url.ts
@@ -1,5 +1,6 @@
 export const getApiUrl = (path: string): string => {
-  return `http://172.31.32.80:5000/api/${path}`
+  return `http://ec2-18-185-117-191.eu-central-1.compute.amazonaws.com/api/${path}`
 }
 
-export const WSUrl = "http://172.31.32.80:4000/ws"
+export const WSUrl =
+  "http://ec2-18-185-117-191.eu-central-1.compute.amazonaws.com/ws"
