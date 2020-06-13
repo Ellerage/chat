10c34ffec6cc3866c96d4307c973429ebccde132
Artem164 <artem.lomaev@bravedevelopers.com>
2020-06-13 23:14:04 +0500

    remove message controller

--- api/src/messages/messages.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('api/messages')
-export class MessagesController {
-  @Get()
-  async getMessages() {
-    return 'test string';
-  }
-}
