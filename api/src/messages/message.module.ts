aeccae3242aebc2fa4204c45ed2122a23a435b82
Artem164 <artem.lomaev@bravedevelopers.com>
2020-06-13 23:14:35 +0500

    remove message service

--- api/src/messages/message.module.ts
+++ api/src/messages/message.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MessageRepository } from './message.repository';
-import { MessagesController } from './messages.controller';
-import { MessagesService } from './messages.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MessageRepository])],
-  controllers: [MessagesController],
-  providers: [MessagesService],
 })
 export class MessageModule {}
