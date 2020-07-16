a8c18f25aafc34bcb1a6cb3b90e222e58e6c0601
Artem164 <artem.lomaev@bravedevelopers.com>
2020-07-16 22:55:45 +0500

    add docker compose dev

--- web/src/core/socket.ts
+++ web/src/core/socket.ts
@@ -1,4 +1,18 @@
 import socketIOClient from "socket.io-client"
 import { WSUrl } from "./get-api-url"
 
-export const WS = socketIOClient(WSUrl)
+export class Socket {
+  private static instance: SocketIOClient.Socket
+
+  private constructor() {}
+
+  public static getInstance(): any {
+    if (!Socket.instance) {
+      Socket.instance = socketIOClient(WSUrl)
+    }
+
+    return Socket.instance
+  }
+}
+
+export const WS = Socket.getInstance()
