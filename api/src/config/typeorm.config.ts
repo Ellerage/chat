f5238fbc5d2fdb77f468978fe2685ebaff9c0002
Artem164 <artem.lomaev@bravedevelopers.com>
2020-08-23 22:33:50 +0500

    fix package version

--- api/src/config/typeorm.config.ts
+++ api/src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'postgres',
+  host: 'localhost',
   port: 5432,
   username: 'postgres',
   password: '214470',
   database: 'chat',
   entities: [__dirname + '/../**/*.entity.js'],
-  synchronize: false,
+  synchronize: true,
 };
